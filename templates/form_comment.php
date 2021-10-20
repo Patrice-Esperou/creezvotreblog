39f7e3f8adce3a8b14beef74d4ce59f67fa9c5b6
parice esperou <pesperou@gmail.com>
2021-10-20 16:46:57 +0200

    update

--- templates/form_comment.php
+++ templates/form_comment.php
@@ -1,6 +1,6 @@
 
-
-<form method="post" action="../admin_Valid_comment.php?route=getComments=<?= htmlspecialchars($article->getId()); ?>">
+<!-- champ du FORMULAIRE QUI SE TROUVE SUR LA PAGE AJOUTER UN COMMENTAIRE le formulaire est sur single.php-->
+<form method="post" action="index.php">
     <label for="pseudo">Pseudo</label><br>
     <input type="text" id="pseudo" name="pseudo"><br>
     <label for="content">Message</label><br>
